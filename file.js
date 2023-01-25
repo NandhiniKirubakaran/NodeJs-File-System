9438ee07584e2848eb15e2065738b7c33aec3ec6
Nandhini Kirubakaran <knaphasri@gmail.com>
2023-01-25 18:36:29 +0530

    nodejs file system

---
+++ file.js
@@ -0,0 +1,12 @@
+
+// Read file
+const fs = require("fs");
+fs.readFile("./Task/25-01-2023-18.16.txt", "utf-8", (err, data) => {
+    console.log(data);
+});
+
+
+// const fs = require("fs");
+// fs.readFile("./Task", "utf-8", (err,data) => {
+//     console.log(data);
+// });
