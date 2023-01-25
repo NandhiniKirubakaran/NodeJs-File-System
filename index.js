6f62d7117a6fb0c9d5500f8296863477569c0f45
Nandhini Kirubakaran <knaphasri@gmail.com>
2023-01-25 18:43:18 +0530

    add

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
 });
 
+
+// Read file
+const fs = require("fs");
+fs.readFile("./Task/25-01-2023-18.16.txt", "utf-8", (err, data) => {
+    console.log(data);
+});
+
+
+// const fs = require("fs");
+// fs.readFile("./Task", "utf-8", (err,data) => {
+//     console.log(data);
+// });
+
+
+
+
+
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
